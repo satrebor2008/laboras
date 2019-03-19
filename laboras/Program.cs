ba67c41f2eb9014895d1952e9dfeb4a0f34bf5e8
satrebor2008 <48555181+satrebor2008@users.noreply.github.com>
2019-03-19 20:24:06 +0200

    antras

--- laboras/Program.cs
+++ laboras/Program.cs
@@ -10,6 +10,27 @@ namespace laboras
     {
         static void Main(string[] args)
         {
+            string vardas, pavarde;
+            int n, nd, egz;
+            double final;
+            Console.WriteLine("Iveskite studento varda ");
+            vardas = Console.ReadLine();
+            Console.WriteLine("Iveskite studento pavarde ");
+            pavarde = Console.ReadLine();
+            Console.WriteLine("Iveskite egzamino rezultata ");
+            egz = int.Parse(System.Console.ReadLine());
+            Console.WriteLine("Iveskite namu darbu skaiciu ");
+            n = int.Parse(System.Console.ReadLine());
+            Console.WriteLine("Iveskite namu darbu pazymius ");
+            float suma = 0;
+            for (int i = 0; i < n; i++)
+            {
+                suma += float.Parse(System.Console.ReadLine());
+            }
+            final = 0.3 * (suma / n) + 0.7 * egz;
+            Console.WriteLine("{0}{1,10}{2,18}", "Vardas", "Pavarde", "Galutinis (vid.)");
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("{0}{1,10}{2,15}", vardas, pavarde, final);
         }
     }
 }
