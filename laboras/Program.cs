962983d61f6517151a913ea96b1833bb1f4ae23c
satrebor2008 <48555181+satrebor2008@users.noreply.github.com>
2019-03-19 20:37:21 +0200

    trecias

--- laboras/Program.cs
+++ laboras/Program.cs
@@ -8,6 +8,26 @@ namespace laboras
 {
     class Program
     {
+        public static float GetMedian(int[] mas)
+        {
+            Array.Sort(mas);
+            int count = mas.Length;
+            if (count == 0)
+            {
+                throw new InvalidOperationException("Empty collection");
+            }
+            else if (count % 2 == 0)
+            {
+                float a = mas[count / 2 - 1];
+                float b = mas[count / 2];
+                return (a + b) / 2;
+            }
+            else
+            {
+                return mas[count / 2];
+            }
+        }
+
         static void Main(string[] args)
         {
             string vardas, pavarde;
@@ -21,16 +41,35 @@ namespace laboras
             egz = int.Parse(System.Console.ReadLine());
             Console.WriteLine("Iveskite namu darbu skaiciu ");
             n = int.Parse(System.Console.ReadLine());
-            Console.WriteLine("Iveskite namu darbu pazymius ");
-            float suma = 0;
-            for (int i = 0; i < n; i++)
+            Console.WriteLine("1. = Galutinis (vid.) \n2. = Galutinis (med.)");
+            int nse = int.Parse(System.Console.ReadLine());
+
+            switch (nse)
             {
-                suma += float.Parse(System.Console.ReadLine());
+                case 1:
+                    Console.WriteLine("Iveskite namu darbu pazymius ");
+                    float suma = 0;
+                    for (int i = 0; i < n; i++)
+                    {
+                        suma += float.Parse(System.Console.ReadLine());
+                    }
+                    final = 0.3 * (suma / n) + 0.7 * egz;
+                    Console.WriteLine("{0}{1,10}{2,18}", "Vardas", "Pavarde", "Galutinis (vid.)");
+                    Console.WriteLine("----------------------------------");
+                    Console.WriteLine("{0}{1,10}{2,15}", vardas, pavarde, final);
+                    break;
+                case 2:
+                    Console.WriteLine("Iveskite namu darbu pazymius ");
+                    int[] mas = new int[n];
+                    for (int i = 0; i < mas.Length; i++)
+                    {
+                        mas[i] = int.Parse(Console.ReadLine());
+                    }
+                    Console.WriteLine("{0}{1,10}{2,18}", "Vardas", "Pavarde", "Galutinis (med.)");
+                    Console.WriteLine("----------------------------------");
+                    Console.WriteLine("{0}{1,10}{2,15}", vardas, pavarde, 0.3 * GetMedian(mas) + 0.7 * egz);
+                    break;
             }
-            final = 0.3 * (suma / n) + 0.7 * egz;
-            Console.WriteLine("{0}{1,10}{2,18}", "Vardas", "Pavarde", "Galutinis (vid.)");
-            Console.WriteLine("----------------------------------");
-            Console.WriteLine("{0}{1,10}{2,15}", vardas, pavarde, final);
         }
     }
 }
